dcb999d6a7bf2c0df38227e4c29c4582762b90c1
Luis Henrique <143908125+luisz19@users.noreply.github.com>
2023-12-13 17:35:27 -0300

    Update script.js
    addeda function for menu and change of images

--- e-commerce-product-page/ecommerce-product-page-main/script.js
+++ e-commerce-product-page/ecommerce-product-page-main/script.js
@@ -5,6 +5,7 @@ const btnBuy = document.getElementById('btnBuy');
 const btnExit = document.getElementById('exit');
 const cartScreen = document.getElementById('cart-screen');
 
+//functions counter
 let count = 0;
 let intervalId = null;
 
@@ -36,7 +37,9 @@ minusButton.addEventListener('mousedown', () => {
 });
 
 document.addEventListener('mouseup', stopInterval);
+//functions counter
 
+//functions cart screen
 btnBuy.addEventListener('click', function () {
     if (cartScreen.style.display === 'none' || cartScreen.style.display === '') {
         cartScreen.style.display = 'block';
@@ -48,5 +51,42 @@ btnBuy.addEventListener('click', function () {
 btnExit.addEventListener('click', function () {
     if (cartScreen.style.display === 'block') {
         cartScreen.style.display = 'none';
+    }
+});
+//functions cart screen
+
+// functions images
+const btnImg1 = document.getElementById('radio1');
+const btnImg2 = document.getElementById('radio2');
+const btnImg3 = document.getElementById('radio3');
+const btnImg4 = document.getElementById('radio4');
+
+btnImg1.addEventListener('click', function (){
+    document.getElementById('img-change').src="images/image-product-1.jpg"
+})
+
+btnImg2.addEventListener('click', function (){
+    document.getElementById('img-change').src="images/image-product-2.jpg"
+})
+
+btnImg3.addEventListener('click', function (){
+    document.getElementById('img-change').src="images/image-product-3.jpg"
+})
+
+btnImg4.addEventListener('click', function (){
+    document.getElementById('img-change').src="images/image-product-4.jpg"
+})
+
+// functions images
+
+const hamburger = document.getElementById('btn-menu');
+const header = document.getElementById('header');
+
+hamburger.addEventListener('click', function () {
+    if(header.style.height === '60px'){
+        header.style.height = '140px';
+    }
+    else {
+        header.style.height = '60px';
     }
 });
